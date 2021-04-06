9592e806bfc44b23bf035d1d0d91c61afd0d2f4c
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-04-06 17:01:19 +0200

    gRPC Services

---
+++ HotChanApi/Services/NewPostService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+
+namespace HotChanApi.Services
+{
+	public class NewPostService
+	{
+	
+	}
+
+	//public override Task<WeatherReply>
+}
---
+++ HotChanApi/Services/PostViewService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using HotChanApi.Data;
+using HotChanShared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotChanApi.Services
+{
+	public class PostViewService : Chan.ChanBase
+	{
+		private readonly DataContext _db;
+
+		public PostViewService(DataContext context)
+		{
+			context = this._db;
+		}
+
+		// All types are from HotChanShared.Models and not to be confused with types in HotChanApi
+		public override Task<PostReply> PostQuery(PostIdRequest request, ServerCallContext context)
+		{
+			var reply = new PostReply { };
+			var post = _db.Posts.FirstOrDefaultAsync(x => x.PostId.ToString() == request.PostId.ToString());
+			
+			return Task.FromResult(reply);
+		}
+	}
+}
