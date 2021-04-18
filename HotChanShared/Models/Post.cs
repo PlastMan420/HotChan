6919f21f5d9626030024f287f9cbd20155666e93
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-04-19 00:12:12 +0200

    shared models between server and client

---
+++ HotChanShared/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace HotChanShared.Models
+{
+	public class Post
+	{
+		[Key]
+		[Column(TypeName = "bigint")]
+		public ulong PostId { get; set; }
+
+		[Required]
+		[Column(TypeName = "bigint")]
+		public ulong	UserId		{ get; set; }
+
+		[Required] [MaxLength(20)]
+		public	string		PostTitle	{ get; set; }
+		public	string		Tags		{ get; set; }
+		public	string		Description	{ get; set; }
+		[Required]
+		public	DateTime	Time		{ get; set; }
+		[Required]
+		public	Uri			MediaUrl	{ get; set; }
+
+		//public string InternalReplyIds	{ get; set; }
+		
+	
+
+	}
+}
---
+++ HotChanShared/Models/Reply.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotChanShared.Models
+{
+	[Index(nameof(ReplyId), IsUnique = true)]
+	public class Reply
+	{
+		[Key]
+		public ulong ReplyId				{ get; set; }
+		[Required]
+		public ulong PostId				{ get; set; }
+		[Required]
+		public ulong UserId				{ get; set; }
+		[Required]
+		public string	Comment				{ get; set; }
+		[Required]
+		public DateTime Time				{ get; set; }
+		[Required]
+		public Uri		AvatarThumbnailUrl	{ get; set; }
+	}
+}
