72b11532cd1618cdf79c3e3d0a3c07c7f0555c69
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-04-19 00:25:12 +0200

    [API][CHORE] renamed grpc messages. sending a hardcoded message for mocking. removed user replies for now

--- HotChanApi/Services/PostViewService.cs
+++ HotChanApi/Services/PostViewService.cs
@@ -3,27 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+
 using HotChanApi.Data;
 using HotChanShared.Models;
-using Microsoft.EntityFrameworkCore;
+
+using AutoMapper;
+using Grpc.Core;
+using System.Diagnostics;
 
 namespace HotChanApi.Services
 {
-	public class PostViewService : Chan.ChanBase
+	public class PostViewService : ChanPostView.ChanPostViewBase
 	{
 		private readonly DataContext _db;
+		private readonly IMapper _mapper;
 
-		public PostViewService(DataContext context)
+
+		public PostViewService(DataContext context, IMapper mapper)
 		{
-			context = this._db;
+			_db = context;
+			_mapper = mapper;
+
 		}
 
 		// All types are from HotChanShared.Models and not to be confused with types in HotChanApi
 		public override Task<PostReply> PostQuery(PostIdRequest request, ServerCallContext context)
 		{
-			var reply = new PostReply { };
-			var post = _db.Posts.FirstOrDefaultAsync(x => x.PostId.ToString() == request.PostId.ToString());
+			//var post = new Post();
+			var reply = new PostReply { 
+				PostId = 26,
+				UserId = 26,
+				PostTitle = "hello",
+				PostTime=Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow),
+				PostDescription="lorem lipsum",
+				MediaUrl= "http://dummyimage.com/178x146.png/cc0000/ffffff"
+			};
+			//post = _db.Posts.FirstOrDefault(x => x.PostId == request.PostId);
+			//reply = _mapper.Map<PostReply>(post);
+			
+			Debug.WriteLine(reply.PostTitle);
 			
 			return Task.FromResult(reply);
 		}
