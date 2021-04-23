2a53380386f22506db9b33ea1c8c7353cf9d8647
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-04-23 22:05:30 +0200

    [FEAT] JWT Tokens [FEAT] Moved to Postgres

--- HotChanApi/Startup.cs
+++ HotChanApi/Startup.cs
@@ -1,13 +1,18 @@
 ﻿using HotChanApi.Data;
+using HotChanApi.Models;
 using HotChanApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace HotChanApi
 {
@@ -42,8 +47,35 @@ namespace HotChanApi
 					   .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
 			}));
 
+			// Connection String is in the secrets storage.
+			string PostGresConnectionString = Configuration["ConnectionStrings:hotchandatabase-postgres-dev"];
 			services.AddDbContext<DataContext>(options =>
-				options.UseSqlServer(Configuration.GetConnectionString("hotchandatabase")));
+				options.UseNpgsql(Configuration.GetConnectionString(PostGresConnectionString)));
+
+			// AddIdentity: for server-side razor pages.
+			// AddIDentityCore: same as add Identity without server side razor pages.
+			IdentityBuilder builder = services.AddIdentityCore<User>();
+
+			// We Want to Query the users and their roles at the same time
+			builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+			// Since we're using AddIdentityCore. We will have to add/configure this manually =>
+			builder.AddEntityFrameworkStores<DataContext>();
+			builder.AddRoleValidator<RoleValidator<Role>>();
+			builder.AddRoleManager<RoleManager<Role>>();
+			builder.AddSignInManager<SignInManager<User>>();
+
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+						.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});          
+			//
+
 
 			//services.AddScoped<IThreadBox, ThreadBox>();
 		}
