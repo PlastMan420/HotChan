73f626596c4a056e1587fe7a4320493aed132b8b
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-07-15 22:33:39 +0200

    [FEAT] new query type for fetching by ID

---
+++ HotChan.DataAccess/Data/IPostIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using HotChan.DataBase;
+using HotChan.DataBase.Models;
+using System;
+
+namespace HotChan.DataAccess.Data
+{
+	public interface IPostIdQuery
+	{
+		public Post GetPostById(HotChanContext context, Guid PostId);
+	}
+}
---
+++ HotChan.DataAccess/Data/PostIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using HotChan.DataBase;
+using HotChan.DataBase.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChan.DataAccess.Data
+{
+	public class PostIdQuery : IPostIdQuery
+	{
+		[UseDbContext(typeof(HotChanContext))]
+		public Post GetPostById([ScopedService] HotChanContext context, [GraphQLName("PostId")] Guid postId)
+		=> context.Posts.FirstOrDefault(a => a.PostId == postId);
+	}
+}
