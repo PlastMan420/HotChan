155a26809af4a4cbbd769f68784faf697ea942f0
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-07-18 16:42:14 +0200

    [FEAT] UsersQuery and Users data loader

---
+++ HotChan.DataAccess/Users/UserQuery.cs
@@ -0,0 +1,38 @@
+﻿using HotChan.DataAccess.DataLoader;
+using HotChan.DataAccess.Filters;
+using HotChan.DataBase;
+using HotChan.DataBase.Extensions;
+using HotChan.DataBase.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotChan.DataAccess.Users
+{
+	[ExtendObjectType(Name = "Query")]
+	public class UserQuery
+	{
+		public IQueryable<User> GetUsers(
+			[ScopedService] HotChanContext hotchanContext) =>
+			hotchanContext.Users;
+
+		[UseApplicationDbContext]
+		public Task<User> GetUserAsync(
+			UsersDL userIdDl,
+			CancellationToken cancellationToken,
+			[GraphQLName("UserId")] Guid userId
+			)
+			=> userIdDl.LoadAsync(userId, cancellationToken);
+
+		[UseApplicationDbContext]
+		public async Task<List<User>> GetUsersAsync([ScopedService] HotChanContext hotchanContext)
+			=> await hotchanContext.Users.ToListAsync();
+	}
+}
