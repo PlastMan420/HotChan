31df87cb6a88eabca717ec3057545b70425ff318
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-07-28 11:21:10 +0200

    [CHORE] change data annotation placing

--- HotChan.DataBase/Models/User.cs
+++ HotChan.DataBase/Models/User.cs
@@ -2,24 +2,25 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace HotChan.DataBase.Models
 {
 	public class User : IdentityUser<Guid>
 	{
+		[PersonalData]
 		public DateTimeOffset RegisterationDate { get; set; }
 		public DateTimeOffset LastOnline { get; set; }
 		public DateTimeOffset DOB { get; set; }
 
-		[PersonalData]
 		public Uri Avatar { get; set; }
 
 		public ICollection<UserRole> UserRoles { get; set; }
 		public ICollection<Post> Posts { get; set; }
 		public ICollection<Post> Favorits { get; set; }
+
+		public string Salt { get; set; }
 
 	}
 }
