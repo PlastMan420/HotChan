e6d19766fad3a93bb2a72fa4d70b6c742631cda6
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-08-21 21:54:47 +0200

    [FEAT] Create user mutation

---
+++ HotChan.DataAccess/Users/CreateUserMutation.cs
@@ -0,0 +1,41 @@
+﻿using HotChan.DataAccess.Repository;
+using HotChan.DataBase;
+using HotChan.DataBase.Extensions;
+using HotChan.DataBase.Models;
+using HotChocolate;
+using System;
+using System.Threading.Tasks;
+
+namespace HotChan.DataAccess.Users
+{
+	class CreateUserMutation
+	{
+		private readonly IUserRepository _userRepo;
+		public CreateUserMutation(IUserRepository userRepo)
+		{
+			_userRepo = userRepo;
+		}
+
+		[UseApplicationDbContext]
+		public async Task<UserAddedPayload> RegisterUser(
+			[ScopedService] HotChanContext hotchanContext,
+			string userName,
+			string email,
+			string password
+		)
+		{
+			var user = new User
+			{
+				UserName = userName,
+				NormalizedUserName = userName.ToLower(),
+				Email = email,
+				NormalizedEmail = email.ToLower(),
+				RegisterationDate = DateTimeOffset.Now,
+			};
+
+			await _userRepo.CreateUser(user, password);
+
+			return new UserAddedPayload { CreationTimeOffset = user.RegisterationDate, UserName = user.UserName };
+		}
+	}
+}
