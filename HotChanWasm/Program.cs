809790fe84e2687fb437f9fd442cbe4e8de787b0
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-08-21 21:58:23 +0200

    [FEAT] replace older gRPC stuff with GQL stuff

--- HotChanWasm/Program.cs
+++ HotChanWasm/Program.cs
@@ -10,10 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
-
-using HotChanShared.Models;
 
 namespace HotChanWasm
 {
@@ -23,26 +19,14 @@ namespace HotChanWasm
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
-			
-			// gRPC client =>
-			builder.Services.AddSingleton(services =>
-			{
-				#if DEBUG
-					var backendurl = "https://localhost:5001"; // local debug url
-				#else
-					var backendurl = "https://some.external.url:12345"; // production url
-				#endif
 
-				// Create a channel with a GrpcWebHandler that is addressed to the backend server.
-				//
-				// GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
-				// then GrpcWeb is recommended because it produces smaller messages.
-				var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+			builder.Services.AddScoped(sp => 
+				new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+			builder.Services
+			.AddHotChanWasmClient()
+			.ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:5001/graphql"));
 
-				return GrpcChannel.ForAddress(backendurl, new GrpcChannelOptions { HttpHandler = httpHandler });
-				//return new chanpostview.chanpostviewclient(channel);
-			});
 
 			await builder.Build().RunAsync();
 		}
