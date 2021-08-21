45c6c48b72860d60aa3788b6417d5f17bb2c50a1
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-08-21 21:56:16 +0200

    [FEAT] Users repo

---
+++ HotChan.DataAccess/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using HotChan.DataBase.Models;
+
+namespace HotChan.DataAccess.Repository
+{
+	public interface IUserRepository
+	{
+		public Task<User> CreateUser(User newUser, string password);
+	}
+}
---
+++ HotChan.DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using HotChan.DataBase.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotChan.DataAccess.Repository
+{
+	public class UserRepository : IUserRepository
+	{
+		private readonly UserManager<User> _userManager;
+
+		public UserRepository(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+
+		}
+
+		public async Task<User> CreateUser(User newUser, string password)
+		{
+			var salt = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("=", "").Replace("+", "-").Replace("/", "_");
+
+			await _userManager.CreateAsync(newUser, password + salt);
+			await _userManager.AddToRoleAsync(newUser, "Member");
+			
+			return newUser;
+		}
+
+		// TODO. Login
+		//public async Task<User> SignIn(string username, string password)
+		//{
+		//	_userManager.lo
+		//}
+	}
+}
