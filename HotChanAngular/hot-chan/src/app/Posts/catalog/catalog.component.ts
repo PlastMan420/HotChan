b5464a1b2d38e6f49b626f05d9cfbbf31dd89474
PlastMAn420 <mostafa.alaa78@outlook.com>
2023-01-27 15:52:41 +0200

    Post Catalog gql

--- HotChanAngular/hot-chan/src/app/Posts/catalog/catalog.component.ts
+++ HotChanAngular/hot-chan/src/app/Posts/catalog/catalog.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { firstValueFrom, Observable, takeUntil } from 'rxjs';
+import { Post } from 'src/app/Internet/Types';
+
+const CATALOG = gql`
+query HotChanQuery {
+  postCatalog{
+    postId,
+    postTitle,
+    description,
+    thumbnailUrl
+  }
+}
+`;
 
 @Component({
   selector: 'app-catalog',
@@ -6,10 +20,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
+  destroy$: Observable<any> = new Observable<any>;
 
-  constructor() { }
-
+  constructor(private apollo: Apollo) { }
+  catalog!: Post[];
+  
   ngOnInit() {
+    this.getCatalog();
   }
 
+  async getCatalog(){
+    await this.getPostCatalogGql();
+  }
+
+  async getPostCatalogGql() {
+    const source$ = this.apollo
+      .watchQuery<any>({
+        query: CATALOG,
+      })
+      .valueChanges.pipe(takeUntil(this.destroy$));
+
+    try {
+      const response = await firstValueFrom(source$);
+      console.log(response);
+
+      this.catalog = response.data.postCatalog;
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
